19906d34379158f5ef205d4c34fbf2b2f1b93877
Paul <emailme.paul@gmail.com>
2020-03-31 21:36:27 -0400

    389

---
+++ 389.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        ind = list(range(len(t)))
+        for i in s:
+            ind[t.index(i)] = 0
+            t = t.replace(i,'0',1)
+        return t[sum(ind)]
