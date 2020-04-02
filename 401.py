730cd54c9dfc89a96bd31055fdd49e7a22a48e9d
Paul <emailme.paul@gmail.com>
2020-04-02 12:56:52 -0400

    401

---
+++ 401.py
@@ -0,0 +1,23 @@
+class Solution:
+    def readBinaryWatch(self, num: int) -> List[str]:
+        from itertools import combinations
+        works = []
+        for comb in combinations(range(10),num):
+            h = 0
+            m = 0
+            for n in range(4):
+                if n in comb:
+                    h += 2 ** n
+            if h > 11:
+                continue
+            for n in range(4,10):
+                if n in comb:
+                    m += 2 ** (n - 4)
+            if m >= 60:
+                continue
+            if m < 10:
+                m = '0' + str(m)
+            else:
+                m = str(m)
+            works.append(str(h) + ':' + m)
+        return works
