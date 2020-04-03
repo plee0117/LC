351b90d196d78a1b034b2b3229565169f72665b0
Paul <emailme.paul@gmail.com>
2020-04-03 12:09:46 -0400

    414

---
+++ 414.py
@@ -0,0 +1,24 @@
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        def compare(maxes,num):
+            i = 0
+            if num in maxes:
+                return maxes
+            
+            while i < len(maxes):
+                if num > maxes[i]:
+                    maxes.insert(i,num)
+                    return maxes[:3]
+                else:
+                    i += 1
+            if len(maxes) < 3:
+                maxes += [num]
+            return maxes[:3]
+        maxes = []
+        for num in nums:
+            maxes = compare(maxes,num)
+            print(maxes)
+        if len(maxes) == 3:
+            return maxes[-1]
+        else:
+            return maxes[0]
