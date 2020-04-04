e62012ca9989b3ee95aa36ef7ed9f99a6d1ae27e
Paul <emailme.paul@gmail.com>
2020-04-04 13:07:20 -0400

    13

---
+++ 13.py
@@ -0,0 +1,11 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        conv = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        nums = 0
+        for i in range(len(s)-1):
+            if conv[s[i]] < conv[s[i+1]]:
+                nums -= conv[s[i]]
+            else:
+                nums += conv[s[i]]
+        nums += conv[s[-1]]
+        return nums
