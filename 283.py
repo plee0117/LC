a0271605748bc142f9bd5def8aec7ba0e69b3070
Paul <emailme.paul@gmail.com>
2020-04-21 18:24:41 -0400

    283

---
+++ 283.py
@@ -0,0 +1,8 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in range(1,len(nums)+1):
+            if nums[-i] == 0:
+                nums.append(nums.pop(-i))
