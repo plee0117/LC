c32ee5636970006372c97d1bfe2a8caa06c7bfc5
Paul <emailme.paul@gmail.com>
2020-05-09 13:35:33 -0400

    475

---
+++ 475.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findRadius(self, houses: List[int], heaters: List[int]) -> int:
+        biggest = 0
+        jdx = 0
+        last = len(heaters) - 1
+        for house in houses:
+            while house > heaters[jdx]:
+                if jdx < last:
+                    jdx += 1
+                else:
+                    return max(biggest, houses[-1] - heaters[-1])
+            if jdx == 0:
+                temp = heaters[jdx] - house
+            else:
+                temp = min(house - heaters[jdx - 1], heaters[jdx] - house)
+            biggest = max(biggest, temp)
+        return biggest
